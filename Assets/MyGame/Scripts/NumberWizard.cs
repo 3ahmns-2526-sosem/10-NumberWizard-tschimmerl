86136288e81e554dc2b5b868c7d4429876dea0f1
Tanja <tanja@Laptop_Tanja.com>
2026-06-10 14:38:08 +0200

    add correct answer state

--- Assets/MyGame/Scripts/NumberWizard.cs
+++ Assets/MyGame/Scripts/NumberWizard.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using UnityEngine.UI; 
 
 public class NumberWizard : MonoBehaviour
 {
-    
     [SerializeField] private int min = 1;
     [SerializeField] private int max = 100;
 
-    
     [SerializeField] private TMP_Text guessText;
+
+    
+    [SerializeField] private Button higherButton;
+    [SerializeField] private Button lowerButton;
+    [SerializeField] private Button correctButton;
 
     private int guess;
 
@@ -19,40 +23,44 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-        
         guess = (min + max) / 2;
-
-        
         UpdateGuessDisplay();
     }
 
-    
     public void OnHigherPressed()
     {
-        
         min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnLowerPressed()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    
+    public void OnCorrectPressed()
+    {
+        
+        Debug.Log("I guessed your number!");
 
         
-        NextGuess();
+        if (guessText != null)
+        {
+            guessText.text = $"Win! {guess}";
+        }
+
+        
+        if (higherButton != null) higherButton.interactable = false;
+        if (lowerButton != null) lowerButton.interactable = false;
+        if (correctButton != null) correctButton.interactable = false;
     }
 
     void NextGuess()
     {
-        
         guess = (min + max) / 2;
-
-        
         UpdateGuessDisplay();
-    }
-
-   
-    public void OnLowerPressed()
-    {
-        
-        max = guess - 1;
-
-        
-        NextGuess();
     }
 
     void UpdateGuessDisplay()
